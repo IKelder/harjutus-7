fb3086bed03f59685a2808b690fc4dd6c9deccca
Ingrid <Kelder>
2014-09-23 10:53:25 +0300

    While loop

--- index.php
+++ index.php
@@ -5,5 +5,22 @@
 <title>Harjutus 7</title>
 </head>
 <body>
+<h2>While loop</h2>
+<?php
+    $count = 10;
+
+    while ($count <= 100) {
+        echo $count . ", ";
+        $count += 1;
+    }
+?> <br><br>
+<?php
+    $count = 100;
+
+    while ($count <= 1000) {
+        echo $count . ", ";
+        $count += 100;
+    }
+?>
 </body>
 </html>
